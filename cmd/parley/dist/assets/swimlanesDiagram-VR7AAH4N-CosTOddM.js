9586bb6548ce5c6828d6b21f775be5fed8700599
Kasra Rasaee <krasaee@gmail.com>
2026-09-17 06:04:55 -0700

    console: rebuild the embedded viewer (includes #19's artifact kind) for v0.3.11

---
+++ cmd/parley/dist/assets/swimlanesDiagram-VR7AAH4N-CosTOddM.js
@@ -0,0 +1,8 @@
+import{c as r,s as e}from"./flowDiagram-HODETNUW-DLGG4cs8.js";import{_ as a}from"./mermaid.core-DEqQaBGU.js";import"./chunk-5VM5RSS4-CiQ_6Fd0.js";import"./chunk-XXDRQBXY-C1Ik5Yw0.js";import"./chunk-POPQ4Y6H-B136Ud1d.js";import"./chunk-F27PBJKO-D4B-MwUl.js";import"./index-MG_55SwT.js";import"./channel-CZnYAT64.js";var o=a(t=>`${e(t)}
+  .swimlane.cluster rect {
+    stroke: ${t.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,"getStyles"),s=o,n=r({defaultLayout:"swimlane",styles:s});export{n as diagram};
