9b71acd318fa61b5750bdc43e983787a703862c8
Kasra Rasaee <krasaee@gmail.com>
2026-09-18 16:25:10 -0700

    console: any number of resizable panes and a resizable sidebar, on terminal-ux-components TerminalSplit; v0.3.16

--- cmd/parley/dist/assets/swimlanesDiagram-VR7AAH4N-C7JbCGsQ.js
+++
@@ -1,8 +0,0 @@
-import{c as r,s as e}from"./flowDiagram-HODETNUW-Dynr1WLe.js";import{_ as a}from"./mermaid.core-DHZ6MMs4.js";import"./chunk-5VM5RSS4-aHUXV2MW.js";import"./chunk-XXDRQBXY-DtzQPHR3.js";import"./chunk-POPQ4Y6H-WQTWZW5l.js";import"./chunk-F27PBJKO-BE9VBTG9.js";import"./index-CDI_jp_o.js";import"./channel-D52DYecp.js";var o=a(t=>`${e(t)}
-  .swimlane.cluster rect {
-    stroke: ${t.clusterBorder} !important;
-  }
-  [data-look="neo"].cluster rect {
-    filter: none;
-  }
-`,"getStyles"),s=o,n=r({defaultLayout:"swimlane",styles:s});export{n as diagram};
