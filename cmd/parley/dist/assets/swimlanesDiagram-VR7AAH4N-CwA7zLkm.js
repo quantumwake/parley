ac9ef136dc8a4e4c7d6af084dcd3eba28048d654
The Bored Engineer <krasaee@gmail.com>
2026-09-20 16:01:28 -0700

    console: make reply a visible button at the bottom of the card
    Champion: merge after focus-ring re-check. Owner: push after tested.

--- cmd/parley/dist/assets/swimlanesDiagram-VR7AAH4N-CwA7zLkm.js
+++
@@ -1,8 +0,0 @@
-import{c as r,s as e}from"./flowDiagram-HODETNUW-CoUkEWZ2.js";import{_ as a}from"./mermaid.core-BT9ZrPxp.js";import"./chunk-5VM5RSS4-C8EJdk0a.js";import"./chunk-XXDRQBXY-CmnBz_C5.js";import"./chunk-POPQ4Y6H-CcLqzS2t.js";import"./chunk-F27PBJKO-9C3XE-XE.js";import"./index-BqUMclY9.js";import"./channel-BhDqSY3Y.js";var o=a(t=>`${e(t)}
-  .swimlane.cluster rect {
-    stroke: ${t.clusterBorder} !important;
-  }
-  [data-look="neo"].cluster rect {
-    filter: none;
-  }
-`,"getStyles"),s=o,n=r({defaultLayout:"swimlane",styles:s});export{n as diagram};
